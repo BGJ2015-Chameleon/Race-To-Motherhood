d9cfdcf5e15ef81b64c62dc53ce2eba515ac7406
Krister Berntsen <krister@probeus.no>
2015-01-24 18:22:56 +0000

    Level 2 - First iteration Ground scrolls, characters animate and gyro works.

---
+++ Assets/Scripts/Levels/Level2/Backround.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Backround : MonoBehaviour {
+	public Sprite[] tiles;
+	SpriteRenderer sr;
+	Transform tr;
+	Camera camera;
+	Vector3 scale;
+
+	// Use this for initialization
+	void Start () {
+
+		camera = Camera.main;
+		sr = GetComponent <SpriteRenderer> ();
+		tr = GetComponent <Transform> ();
+		sr.sprite = tiles [0];
+
+		float cameraHeight = camera.orthographicSize * 2f;
+		float cameraWidth = cameraHeight / Screen.height * Screen.width;
+		scale = new Vector3 (cameraWidth / tiles[0].bounds.size.x, cameraHeight / tiles[0].bounds.size.y, 1f);
+		tr.localScale = scale;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float cameraHeight = camera.orthographicSize * 2f;
+		float cameraWidth = cameraHeight / Screen.height * Screen.width;
+		scale = new Vector3 (cameraWidth / tiles[0].bounds.size.x, cameraHeight / tiles[0].bounds.size.y, 1f);
+		tr.localScale = scale;
+	}
+	
+
+}
---
+++ Assets/Scripts/Levels/Level2/Bike.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bike : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Input.gyro.enabled = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+	float clamp(float v, float min, float max){
+		if (v < min) {
+			return min;
+		} else if (v > max) {
+			return max;
+		}
+		return v;
+	}
+
+	void Move(){
+		Vector3 gyro = Input.gyro.rotationRate;
+		gyro.x = 0;
+		transform.Translate (gyro/10);
+		Vector3 pos = transform.localPosition;
+		float clamped = clamp (pos.y, -2, 2);
+		pos.y = clamped;
+		transform.localPosition = pos;
+	}
+
+}
