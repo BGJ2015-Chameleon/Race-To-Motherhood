5b721c1ee06353ce2bb85e0b510485c7d12a9cce
Krister Berntsen <krister@probeus.no>
2015-01-25 14:08:55 +0000

    Cleaned up between stages.

--- Assets/Level1Level2.cs
+++ Assets/Level1Level2.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class Level1Level2 : MonoBehaviour {
-	GameObject timeGO, hitsGO, NextButton, GoButton, image;
+	GameObject timeGO, hitsGO, NextButton, GoButton, image, timeLabel, hitsLabel, level2Label;
 	public float time;
 	public int hits;
 
@@ -17,8 +17,12 @@ public class Level1Level2 : MonoBehaviour {
 		timeGO = GameObject.Find ("Time");
 		hitsGO = GameObject.Find ("Hits");
 		image = GameObject.Find ("Image");
+		timeLabel = GameObject.Find ("TimeLabel");
+		hitsLabel = GameObject.Find ("HitsLabel");
+		level2Label= GameObject.Find ("Level2Label");
 
 		GoButton.SetActive (false);
+		level2Label.SetActive (false);
 
 		timeGO.GetComponent <Text> ().text = "0:00:00";
 		hitsGO.GetComponent <Text> ().text = "0";
@@ -33,8 +37,13 @@ public class Level1Level2 : MonoBehaviour {
 
 	public void ShowLevel2(){
 		image.GetComponent<Image>().sprite = image2;
+		level2Label.SetActive (true);
 		NextButton.SetActive (false);
 		GoButton.SetActive (true);
+		timeGO.SetActive (false);
+		hitsGO.SetActive (false);
+		timeLabel.SetActive (false);
+		hitsLabel.SetActive (false);
 
 	}
 
